5fdbd9fbabf02a3ff80f2d010ec6d2b13e3c2c51
SerhiiAndruschenko <andruschenko033@gmail.com>
2024-01-12 14:36:37 +0200

    new content for about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,43 @@
 import Heading from "@/src/components/Heading";
+import '@/public/styles/about.scss';
+
 
 export const metadata = {
-  title: 'About',
-}
+  title: "About",
+};
 
 export default function AboutPage() {
-  return <Heading>About Page</Heading>;
+  return (
+    <>
+      <Heading>About Us:</Heading>
+
+      <div className="about-content font-exo2">
+        <p>
+          Welcome to Indie Game Reviews, where we explore and share our thoughts
+          on the latest indie games in the gaming world. Our team of passionate
+          gamers is dedicated to providing honest and insightful reviews to help
+          you discover hidden gems among indie titles.
+        </p>
+
+        <h2>Our Mission</h2>
+        <p>
+          At Indie Game Reviews, our mission is to celebrate the creativity and
+          innovation found in independent games. We believe in supporting indie
+          developers and showcasing their unique contributions to the gaming
+          industry. Whether you're a seasoned gamer or new to indie titles, we
+          strive to guide you toward exciting and memorable gaming experiences.
+        </p>
+
+        <h2>Contact Us</h2>
+        <p>
+          Have a suggestion, question, or want to collaborate? Feel free to
+          reach out to us at{" "}
+          <a href="mailto:contact@indiegamereviews.com">
+            contact@indiegamereviews.com
+          </a>
+          .
+        </p>
+      </div>
+    </>
+  );
 }
