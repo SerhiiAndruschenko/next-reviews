e3845b4b491f97d4418787835ac6d61677e8ed67
SerhiiAndruschenko <andruschenko033@gmail.com>
2024-01-24 20:51:47 +0200

    Added simple pagination

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,38 @@
 import Link from "next/link";
 import Heading from "@/src/components/Heading";
-import { getReviews } from "@/lib/reviews";
+import { getPagedReviews, getPageCount } from "@/lib/reviews";
 
 /*export const metadata = {
   title: "Reviews",
 };*/
 
-export default async function ReviewsPage() {
-  const reviews = await getReviews();
+function parsePageParams(paramValue) {
+  if (paramValue) {
+    const page = parseInt(paramValue);
+    if (isFinite(page) && page > 0) {
+      return page;
+    }
+  }
+  return 1;
+}
+
+export default async function ReviewsPage({ searchParams }) {
+  const page = parsePageParams(searchParams.page);
+  const reviews = await getPagedReviews(page, 5);
+  const pageCount = await getPageCount();
   return (
     <>
       <Heading>Reviews</Heading>
+      <div className="pagination flex gap-2 align-middle mt-3 mb-3">
+        {page >= 2 && <Link href={`/?page=${page - 1}`}>&lt;</Link>}
+
+        <span>Page {page}</span>
+        {page !== pageCount && (
+          <Link href={`/?page=${page + 1}`}>
+            &gt;
+          </Link>
+        )}
+      </div>
       <ul className="flex flex-col gap-4">
         {reviews.map((review) => (
           <li
@@ -23,12 +45,15 @@ export default async function ReviewsPage() {
             >
               <img src={review.image} className="rounded-t w-full sm:w-96" />
               <div className="post-card__content">
-                <p className="font-gentium text-left mb-2">{review.author.name}</p>
+                <p className="font-gentium text-left mb-2">
+                  {review.author.name}
+                </p>
 
-                <h2 className="font-gentium text-left">
-                  {review.title}
-                </h2>
-                <span className="font-gentium text-left" dangerouslySetInnerHTML={{ __html: review.excerpt }} />
+                <h2 className="font-gentium text-left">{review.title}</h2>
+                <span
+                  className="font-gentium text-left"
+                  dangerouslySetInnerHTML={{ __html: review.excerpt }}
+                />
               </div>
             </Link>
           </li>
