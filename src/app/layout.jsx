e5dd5819c53ae322d4335e36f36ef30b1a4fbdc6
SerhiiAndruschenko <andruschenko033@gmail.com>
2024-02-09 10:54:13 +0200

    adsense script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,32 +4,43 @@ import { exo2, orbitron, gentium } from "./fonts";
 import "./globals.css";
 import "@/public/styles/header.scss";
 import "@/public/styles/footer.scss";
-import '@/public/styles/reviews.scss';
-import { Analytics } from '@vercel/analytics/react';
+import "@/public/styles/reviews.scss";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import Image from "next/image";
 import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: {
-    default: 'Bookworm Haven - Uncover Literary Treasures with Insightful Reviews',
-    template: '%s | Bookworm Haven'
+    default:
+      "Bookworm Haven - Uncover Literary Treasures with Insightful Reviews",
+    template: "%s | Bookworm Haven",
   },
-  description: 'Welcome to Bookworm Haven, where the magic of literature comes alive through our passionate team of avid readers. Dive into a world of insightful and comprehensive book reviews, as we celebrate the rich tapestry of both established and emerging authors. Our mission is to be your go-to destination for thoughtful and honest reviews, guiding bibliophiles and casual readers alike to discover the next captivating page-turner. Join us on a journey through the diverse realm of literature, and let Bookworm Haven be your trusted companion in exploring the endless possibilities of storytelling.',
-}
+  description:
+    "Welcome to Bookworm Haven, where the magic of literature comes alive through our passionate team of avid readers. Dive into a world of insightful and comprehensive book reviews, as we celebrate the rich tapestry of both established and emerging authors. Our mission is to be your go-to destination for thoughtful and honest reviews, guiding bibliophiles and casual readers alike to discover the next captivating page-turner. Join us on a journey through the diverse realm of literature, and let Bookworm Haven be your trusted companion in exploring the endless possibilities of storytelling.",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className={`${orbitron.variable} ${exo2.variable} ${gentium.variable}` }>
+    <html
+      lang="en"
+      className={`${orbitron.variable} ${exo2.variable} ${gentium.variable}`}
+    >
       <Head>
-        <meta name="google-adsense-account" content="ca-pub-6358243501215789" />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6358243501215789"
+          crossOrigin="anonymous"
+          strategy="lazyOnload"
+        />
       </Head>
       <body className="flex flex-col min-h-screen bg-">
         <Analytics />
         <header className="px-4 py-3 mx-auto">
-          <NavBar/>
+          <NavBar />
         </header>
         <main className="grow px-4 py-14">{children}</main>
         <footer className="px-4 py-3 text-center text-xs">
