778832f0d264c10366b8c9d03b6509e0885c2e8a
SerhiiAndruschenko <andruschenko033@gmail.com>
2024-02-09 10:17:10 +0200

    adsense

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${orbitron.variable} ${exo2.variable} ${gentium.variable}` }>
       <Head>
-        <meta name="google-adsense-account" content="ca-pub-6358243501215789" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6358243501215789" crossorigin="anonymous"></script>
       </Head>
       <body className="flex flex-col min-h-screen bg-">
         <Analytics />
