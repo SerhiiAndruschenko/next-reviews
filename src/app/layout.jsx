becf48eee5eed27df5d0805fcf0d70128ce8e642
SerhiiAndruschenko <andruschenko033@gmail.com>
2024-02-09 10:09:52 +0200

    adsense meta

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -17,15 +17,13 @@ export const metadata = {
     default: 'Bookworm Haven - Uncover Literary Treasures with Insightful Reviews',
     template: '%s | Bookworm Haven'
   },
-  description: 'Welcome to Bookworm Haven, where the magic of literature comes alive through our passionate team of avid readers. Dive into a world of insightful and comprehensive book reviews, as we celebrate the rich tapestry of both established and emerging authors. Our mission is to be your go-to destination for thoughtful and honest reviews, guiding bibliophiles and casual readers alike to discover the next captivating page-turner. Join us on a journey through the diverse realm of literature, and let Bookworm Haven be your trusted companion in exploring the endless possibilities of storytelling.'
+  description: 'Welcome to Bookworm Haven, where the magic of literature comes alive through our passionate team of avid readers. Dive into a world of insightful and comprehensive book reviews, as we celebrate the rich tapestry of both established and emerging authors. Our mission is to be your go-to destination for thoughtful and honest reviews, guiding bibliophiles and casual readers alike to discover the next captivating page-turner. Join us on a journey through the diverse realm of literature, and let Bookworm Haven be your trusted companion in exploring the endless possibilities of storytelling.',
+  googleAdsenseAccount: "ca-pub-6358243501215789"
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${orbitron.variable} ${exo2.variable} ${gentium.variable}` }>
-      <head>
-        <meta name="google-adsense-account" content="ca-pub-6358243501215789" />
-      </head>
       <body className="flex flex-col min-h-screen bg-">
         <Analytics />
         <header className="px-4 py-3 mx-auto">
