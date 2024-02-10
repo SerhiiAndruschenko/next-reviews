162d0ec892734d765aa3217d334ca5c167e63123
SerhiiAndruschenko <andruschenko033@gmail.com>
2024-02-10 21:35:07 +0200

    Added animatin to banner

--- src/components/HomeBanner.jsx
+++ src/components/HomeBanner.jsx
@@ -1,10 +1,14 @@
+import AnimatedElement from "./AnimatedElement";
+
 export default function HomeBanner() {
   return (
     <>
-      <div className="home-hero">
+      <AnimatedElement>
+        <div className="home-hero">
           <h1 className="font-medievalSharp">Welcome to Bookworm Haven</h1>
           <h2 className="font-medievalSharp">Where Stories Live</h2>
         </div>
+      </AnimatedElement>
     </>
   );
 }
